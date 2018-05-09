d44ce8b1d4bee37035389094b890009b286e91a7
GrassoFederico <gras.federico@gmail.com>
2018-05-10 00:24:04 +0200

    Prima versione funzionante da testare -> aggiungere animazioni

--- fixed.js
+++ fixed.js
@@ -1,28 +1,27 @@
 var offset = 15;
 var finalOffset = 0;
 var originOffset = 0;
+var limitOffset = 0;
 
 $(window).ready(function(){
 	originOffset = $("#banner").offset().top;
+	limitOffset = $("#comments").offset().top;
 });
 
 $(window).scroll(function(){
-	console.log($(this).scrollTop() + " " + $("#banner").offset().top);
+	console.log(($("#banner").offset().top + $("#banner").height()) + " " + $("#comments").offset().top);
 	if($(this).scrollTop() < finalOffset)
 	{
 		if($(this).scrollTop() > originOffset)
-		{
 			offset -= 100;
-		}
 		else
-		{
 			offset = 0;
-		}
 	}
 	if($(this).scrollTop() > $("#banner").offset().top)
 	{
+		if((($("#banner").offset().top + 100) + $("#banner").height()) < limitOffset)
+			offset += 100;
 		finalOffset = $("#banner").offset().top;
-		offset += 100;
 	}
 	$("#banner").css("marginTop",offset + "px");
 });
