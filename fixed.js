2fc70d6c410965362b637065a5a316ac4eb42f2d
GrassoFederico <gras.federico@gmail.com>
2018-05-09 23:47:20 +0200

    Gestito lo scroll e rimane nel parent -> da fixare il fondo

--- fixed.js
+++ fixed.js
@@ -1,20 +1,28 @@
 var offset = 15;
-var dirOffset = 0;
-
-function hide()
-{
-}
-
-function hideBanner(hideStatus)
-{
-}
+var finalOffset = 0;
+var originOffset = 0;
 
 $(window).ready(function(){
+	originOffset = $("#banner").offset().top;
 });
 
 $(window).scroll(function(){
 	console.log($(this).scrollTop() + " " + $("#banner").offset().top);
+	if($(this).scrollTop() < finalOffset)
+	{
+		if($(this).scrollTop() > originOffset)
+		{
+			offset -= 100;
+		}
+		else
+		{
+			offset = 0;
+		}
+	}
 	if($(this).scrollTop() > $("#banner").offset().top)
+	{
+		finalOffset = $("#banner").offset().top;
 		offset += 100;
+	}
 	$("#banner").css("marginTop",offset + "px");
 });
