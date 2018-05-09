7c3672708df3adaaf27b4a74ae6aa1e034df8345
GrassoFederico <gras.federico@gmail.com>
2018-05-09 23:28:07 +0200

    Versione funzionante solo scrollando verso il basso -> implementare direzione opposta e aggiungere transizione

--- fixed.js
+++ fixed.js
@@ -1,4 +1,4 @@
-var offset = 65;
+var offset = 15;
 var dirOffset = 0;
 
 function hide()
@@ -15,17 +15,6 @@ $(window).ready(function(){
 $(window).scroll(function(){
 	console.log($(this).scrollTop() + " " + $("#banner").offset().top);
 	if($(this).scrollTop() > $("#banner").offset().top)
-	{
-		$("#banner").addClass("position-absolute");
-		if(dirOffset < offset)
-		{
-			$("#banner").css("top",offset + "px");
-			offset += 100;
-			//dirOffset = offset;
-		}
-		else
-			offset -= 100;
-	}
-	else
-		$("#banner").removeClass("position-absolute");
+		offset += 100;
+	$("#banner").css("marginTop",offset + "px");
 });
