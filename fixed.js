4444d54cf387f19ee9486b0935b5bb0d623ab0c5
Federico <gras.federico@gmail.com>
2018-05-10 09:58:22 +0200

    Gestito il mouse velocity -> da testare

--- fixed.js
+++ fixed.js
@@ -2,6 +2,10 @@ var offset = 15;
 var finalOffset = 0;
 var originOffset = 0;
 var limitOffset = 0;
+var newPos = null;
+var lastPos = null;
+var delta = 0;
+var istVelocity = 0;
 
 function reset()
 {
@@ -10,6 +14,14 @@ function reset()
 	finalOffset = 0;
 	originOffset = $("#banner").offset().top;
 	limitOffset = $("#comments").offset().top;
+}
+
+function mouseVelocity()
+{
+	newPos = window.scrollY;
+	delta = newPos -  lastPos;
+	lastPos = newPos;
+	return(Math.abs(delta));
 }
 
 $(window).ready(function(){
@@ -21,17 +33,18 @@ $( window ).resize(function() {
 });
 
 $(window).scroll(function(){
+	istVelocity = mouseVelocity();
 	if($(this).scrollTop() < finalOffset)
 	{
 		if($(this).scrollTop() > originOffset)
-			offset -= 100;
+			offset -= istVelocity;
 		else
 			offset = 0;
 	}
 	if($(this).scrollTop() > $("#banner").offset().top)
 	{
-		if((($("#banner").offset().top + 100) + $("#banner").height()) < limitOffset)
-			offset += 100;
+		if((($("#banner").offset().top + istVelocity) + $("#banner").height()) < limitOffset)
+			offset += istVelocity;
 		finalOffset = $("#banner").offset().top;
 	}
 	$("#banner").css("marginTop",offset + "px");
